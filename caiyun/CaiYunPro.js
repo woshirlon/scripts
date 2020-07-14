e915e0402459146140d11042a7922e72983aed6e
QQ515237330 <yangtingxiao@gmail.com>
2020-07-14 13:04:59 +0800

    Create CaiYunPro.js
    caiyun svip beta

---
+++ caiyun/CaiYunPro.js
@@ -0,0 +1,18 @@
+/*
+彩云天气PRO  SVIP
+QX
+[rewrite_local]
+https:\/\/biz\.caiyunapp\.com\/v2\/user\?app_name=weather url script-response-header CaiYunPro.js
+[mitm]
+hostname = biz.caiyunapp.com
+*/
+
+
+var obj =JSON.parse($response.body);
+obj.result.is_vip = true;
+obj.result.vip_type = "s";
+obj.result.wt.vip.enabled = true;
+obj.result.wt.vip.obj.result.wt.vip = 3742762088
+obj.result.svip_expired_at = 3742762088
+$done({body: JSON.stringify(obj)});
+
