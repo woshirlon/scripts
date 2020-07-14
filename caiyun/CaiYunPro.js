24e5cba9a858a93d8ce3738f760eadfa456ca256
QQ515237330 <yangtingxiao@gmail.com>
2020-07-14 13:31:54 +0800

    Update CaiYunPro.js
    fix

--- caiyun/CaiYunPro.js
+++ caiyun/CaiYunPro.js
@@ -12,7 +12,7 @@ var obj =JSON.parse($response.body);
 obj.result.is_vip = true;
 obj.result.vip_type = "s";
 obj.result.wt.vip.enabled = true;
-obj.result.wt.vip.obj.result.wt.vip = 3742762088
+obj.result.wt.vip.svip_expired_at = 3742762088
 obj.result.svip_expired_at = 3742762088
 $done({body: JSON.stringify(obj)});
 
